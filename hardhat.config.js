96f9576b27bd420dbfbeeabf016c3e9a9d3ce904
UBC Student <lakshy01@students.cs.ubc.ca>
2022-03-26 14:13:06 -0700

    changed to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,23 +5,21 @@ const fs = require('fs');
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    /*
+    // hardhat: {
+    //   chainId: 1337
+    // },
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
       url: "https://rpc-mumbai.matic.today",
       accounts: [process.env.privateKey]
     },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [process.env.privateKey]
-    }
-    */
+    // matic: {
+    //   // Infura
+    //   // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+    //   url: "https://rpc-mainnet.maticvigil.com",
+    //   accounts: [process.env.privateKey]
+    // },
   },
   solidity: {
     version: "0.8.4",
